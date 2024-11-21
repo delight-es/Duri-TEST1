6281dab7442caa7bb08581886543cc8582715abe
delight-es <skfmf7878@gmail.com>
2024-11-21 14:00:55 +0900

    bug: spotLess Apply

--- src/main/java/kr/com/duri/controller/HomeController.java
+++ src/main/java/kr/com/duri/controller/HomeController.java
@@ -1,6 +1,5 @@
 package kr.com.duri.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-  @ResponseBody
-  @GetMapping("/duri")
-  public String duri() {
-    String duri = "duri";
-                return duri;
-  }
-
+    @ResponseBody
+    @GetMapping("/duri")
+    public String duri() {
+        String duri = "duri";
+        return duri;
+    }
 }
